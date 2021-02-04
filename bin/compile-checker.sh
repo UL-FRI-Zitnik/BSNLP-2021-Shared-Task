039edbb23b5dba453ea7761bc9c301890a6fe0b0
Marko Prelevikj <pr3mar@users.noreply.github.com>
2021-02-04 12:33:30 +0100

    More details about the consistency checker

--- bin/compile-checker.sh
+++ bin/compile-checker.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 
 # compile the consistency check provided by BSNLP organizers
+
 javac ./src/utils/ConsistencyCheck.java
--- bin/run-checker.sh
+++ bin/run-checker.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 # Takes 2 args:
 #   1. path to directory containing submission files
 #   2. path to file where the report should be generated
+# Example usage [from repo root]:
+#   ./bin/run-checker.sh data/challenge/2021/brexit/annotated/sl/ data/consistency_reports/report_2021-sl-brexit.txt
 
-java ./src/utils/ConsistencyCheck "$@"
+java src/utils/ConsistencyCheck "$@"
